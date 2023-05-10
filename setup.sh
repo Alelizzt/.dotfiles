507c0c415bd9e0c2fe004f2635fea45f647eb413
Iván Lizzt <6182214+Alelizzt@users.noreply.github.com>
2023-05-09 21:12:34 -0500

    fix typo

--- setup.sh
+++ setup.sh
@@ -29,10 +29,10 @@ chmod +x oh-my-zsh.sh
 
 git clone https://github.com/romkatv/powerlevel10k.git ~/.dotfiles/.config/oh-my-zsh/custom/themes/powerlevel10k
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.dotfiles/.config/oh-my-zsh/custom/pluggins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.dotfiles/.config/oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.dotfiles/.config/oh-my-zsh/custom/pluggins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.dotfiles/.config/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-git clone https://github.com/zsh-users/zsh-completions.git ~/.dotfiles/.config/oh-my-zsh/custom/pluggins/zsh-completions
+git clone https://github.com/zsh-users/zsh-completions.git ~/.dotfiles/.config/oh-my-zsh/custom/plugins/zsh-completions
 
 
